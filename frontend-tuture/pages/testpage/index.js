ac4bce243b3858038a3021757d201d577d4a7d53
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-05 03:23:40 +0700

    Add transition fx to TutorCard

--- frontend-tuture/pages/testpage/index.js
+++ frontend-tuture/pages/testpage/index.js
@@ -1,34 +1,41 @@
-import { useState } from "react";
-import { Switch } from "@headlessui/react";
+import { useEffect, useState } from "react";
+import { Switch, Transition } from "@headlessui/react";
 import Layout from "../../components/Layout";
 import TutorCard from "../../components/TutorCard";
 
-const names = ["Adam Benson", "Clare Donaldsdfadsfdsfson", "Phusaratis Jong", "Donald Clarkson"]
-
+const names = [
+  "Adam Benson",
+  "Clare Donaldsdfadsfdsfson",
+  "Phusaratis Jong",
+  "Donald Clarkson",
+];
 
 export default function Testpage() {
   const [enabled, setEnabled] = useState(false);
 
   let temp = [];
   for (let i = 0; i < 4; i++) {
-    temp.push(<TutorCard
-      key={i}
-      name={names[i]}
-      profileImg={'https://www.chicagotribune.com/resizer/a-16fPYl-SK8W6HPnzjOHK1rqho=/800x551/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/IEYVMAFZ7BBXHM46GFNLWRN3ZA.jpg'}
-      subjects={["Physics", "Chemistry"]}
-      levels={["High school", "Middle school"]}
-      rating={Math.random()*5}
-      price={{ min: 300, max: 500 }}
-      onCardClick={() => console.log(`Card click ${names[i]}`)}
-      onChooseClick={() => console.log(`Choose button click ${names[i]}`)}
-    />)    
+    temp.push(
+      <TutorCard
+        key={i}
+        name={names[i]}
+        profileImg={
+          "https://www.chicagotribune.com/resizer/a-16fPYl-SK8W6HPnzjOHK1rqho=/800x551/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/IEYVMAFZ7BBXHM46GFNLWRN3ZA.jpg"
+        }
+        subjects={["Physics", "Chemistry"]}
+        levels={["High school", "Middle school"]}
+        rating={Math.random() * 5}
+        price={{ min: 300, max: 500 }}
+        onCardClick={() => console.log(`Card click ${names[i]}`)}
+        onChooseClick={() => console.log(`Choose button click ${names[i]}`)}
+      />
+    );
   }
 
   return (
     <Layout title={"test page"}>
-      <div className="flex flex-wrap justify-center gap-4 my-2">
-        {temp}
-      </div>
+
+      <div className="flex flex-wrap justify-center gap-4 my-2">{temp}</div>
 
       <Switch.Group>
         <div className="flex items-center">
