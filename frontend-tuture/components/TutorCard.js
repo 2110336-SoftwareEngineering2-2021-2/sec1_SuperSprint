78a2e7fd3a33650f275a2cc03d459b2140c79f99
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-07 20:22:18 +0700

    Using daisyUI on TutorCard

--- frontend-tuture/components/TutorCard.js
+++ frontend-tuture/components/TutorCard.js
@@ -42,43 +42,46 @@ export default function TutorCard({
       leaveTo="opacity-0 scale-95"
     >
       <div
-        className="bg-white dark:bg-zinc-800 shadow-lg hover:shadow-amber-500/50 hover:scale-105 transition-all rounded-lg w-auto max-w-3xl flex justify-between"
+        className="flex w-auto max-w-3xl justify-between rounded-lg bg-white shadow-lg transition-all hover:scale-105 hover:shadow-amber-400/40"
         onClick={onCardClick ?? null}
         tabIndex={0}
       >
         <img
           src={profileImg}
           alt="Profile picture of the tutor"
-          className="rounded-lg sm:w-48 w-24 object-cover flex-1"
+          className="w-24 flex-1 rounded-lg object-cover sm:w-48"
           loading="lazy"
         />
-        <div className="inline-flex flex-col justify-between p-4 sm:w-96 w-64">
-          <h3 className="font-bold text-md dark:text-amber-500 dark:hover:text-white sm:text-xl max-w-fit sm:w-80 xs:w-52 w-40 whitespace-nowrap overflow-hidden overflow-ellipsis hover:text-amber-500 hover:cursor-pointer transition-colors">
+        <div className="inline-flex w-64 flex-col justify-between p-4 sm:w-96">
+          <h3 className="text-md xs:w-52 hover:text-primary-focus w-40 max-w-fit overflow-hidden overflow-ellipsis whitespace-nowrap font-bold transition-colors hover:cursor-pointer sm:w-80 sm:text-xl">
             {name}
           </h3>
-          <p className="sm:text-base text-sm dark:text-white selection:bg-amber-300 cursor-default">
+          <p className="selection:bg-primary cursor-default text-sm sm:text-base">
             <FontAwesomeIcon size="sm" icon={faBookOpen} fixedWidth />{" "}
             {subjects.join(", ")}
           </p>
-          <p className="sm:text-base text-sm dark:text-white selection:bg-amber-300 cursor-default">
+          <p className="selection:bg-primary cursor-default text-sm sm:text-base">
             <FontAwesomeIcon size="sm" icon={faGraduationCap} fixedWidth />{" "}
             {levels.join(", ")}
           </p>
-          <p className="sm:text-base text-sm dark:text-white selection:bg-amber-300 cursor-default">
+          <p className="selection:bg-primary cursor-default text-sm sm:text-base">
             <FontAwesomeIcon size="sm" icon={faDollarSign} fixedWidth />{" "}
             {`${price.min} - ${price.max} baht/hr`}
           </p>
         </div>
-        <div className="inline-flex flex-col justify-between select-none p-2">
+        <div className="inline-flex select-none flex-col justify-between p-2">
           <div>
-            <p className="font-bold text-amber-500 sm:text-4xl text-3xl text-center">
+            <p className="text-primary text-center text-3xl font-bold sm:text-4xl">
               {rating.toFixed(1)}
             </p>
-            <p className="text-amber-500 sm:text-md text-sm text-center">
+            <p className="text-secondary sm:text-md text-center text-sm">
               rating
             </p>
           </div>
-          <button className="btn-primary" onClick={onChoose}>
+          <button
+            className="btn btn-primary btn-sm rounded-full"
+            onClick={onChoose}
+          >
             Choose
           </button>
         </div>
