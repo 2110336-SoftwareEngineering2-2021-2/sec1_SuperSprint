a5ae5524003ebff61e4f085df7f230ea1b644a7b
Peemajoke <zinger.peem@gmail.com>
2022-02-11 00:32:41 +0700

    [sorting component] done

--- frontend-tuture/pages/index.js
+++ frontend-tuture/pages/index.js
@@ -1,9 +1,23 @@
+// import Layout from "../components/Layout";
+
+// export default function Home() {
+//   return (
+//     <Layout title='Home'>
+//       <h1 className="w-full font-bold text-4xl">Hello</h1>
+//     </Layout>
+//   );
+// }
+
 import Layout from "../components/Layout";
+import SortingDropdown from "../components/SortingDropdown";
 
 export default function Home() {
   return (
     <Layout title='Home'>
       <h1 className="w-full font-bold text-4xl">Hello</h1>
+      <div className="mr-0 ml-auto w-fit">
+        <SortingDropdown />
+      </div>
     </Layout>
   );
 }
