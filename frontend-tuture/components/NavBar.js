0b856159b018ad74e5df78ceec44bd0ce3187c8e
Puttinart <puttinart09@gmail.com>
2022-02-11 16:17:53 +0700

    [Navbar searching] reconnect to search result

--- frontend-tuture/components/NavBar.js
+++ frontend-tuture/components/NavBar.js
@@ -3,15 +3,28 @@ import { sideBarData } from './SideBar';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from 'react'
 import Link from 'next/link';
+import { useRouter } from "next/router";
 import {
     faUser
   } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = (props) => {
     const [sideBar, setSideBar] = useState(false);
+    const [tutureName, setTutureName] = useState("");
 
     const toggleSideBar = () => {
         setSideBar(!sideBar);
+    }
+    const router = useRouter();
+    function submitSearching(){
+        router.replace(
+            {
+              path: "",
+              query: {
+                tutureName: tutureName
+                },
+            }
+          );
     }
 
     return (
@@ -37,11 +50,11 @@ const NavBar = (props) => {
 
             <div className="flex-1 justify-end">
             <div className="form-control">
-                <input type="text" placeholder="Search..." className="input text-black"></input>
+                <input type="text" placeholder="Search..." className="input text-black" value={tutureName} onInput={e => setTutureName(e.target.value)}></input>
             </div>
             </div> 
             <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
+            <button className="btn btn-square btn-ghost" onClick={submitSearching}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">             
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" className="stroke-base-100"></path>             
                 </svg>
