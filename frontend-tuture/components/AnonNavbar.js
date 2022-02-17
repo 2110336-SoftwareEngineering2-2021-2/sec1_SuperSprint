f46e7b338959e68a04a30cccd51a73cc7c24c6f1
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-17 23:32:45 +0700

    [frontend] Add dark them switcher to AnonNavbar

--- frontend-tuture/components/AnonNavbar.js
+++ frontend-tuture/components/AnonNavbar.js
@@ -1,25 +1,41 @@
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useTheme } from 'next-themes';
 import Link from 'next/link';
 
 const AnonNavbar = (props) => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <>
       <div className="navbar top-0 z-50 w-full text-neutral-content shadow-md shadow-primary-focus/25">
         <div className="flex-1">
           <Link href="/" passHref>
-            <button className="btn btn-ghost mx-0 text-lg font-bold normal-case text-primary sm:mx-4 xs:text-xl">
+            <button className="btn btn-ghost mx-0 text-lg font-bold normal-case text-primary xs:text-xl sm:mx-4">
               Tuture
             </button>
           </Link>
         </div>
 
         <div className="flex flex-none items-center">
+          <label className="swap swap-rotate btn btn-ghost btn-sm sm:btn-md ">
+            <input
+              type="checkbox"
+              onClick={() =>
+                setTheme(theme === 'default' ? 'default-dark' : 'default')
+              }
+            />
+            <FontAwesomeIcon fixedWidth icon={faSun} className="swap-off" />
+            <FontAwesomeIcon fixedWidth icon={faMoon} className="swap-on" />
+          </label>
+          <div className="divider-horizontal divider"></div>
           <Link href="/login">
-            <a className="w-20 min-w-fit text-center text-sm text-base-content transition-colors hover:text-primary-focus sm:w-24 sm:text-base">
+            <a className="w-20 min-w-fit text-center text-sm normal-case text-base-content transition-colors hover:text-primary-focus sm:w-24 sm:text-base">
               Log In
             </a>
           </Link>
           <Link href="/register">
-            <a className="btn btn-primary btn-sm w-20 normal-case text-sm sm:text-base text-primary-content sm:btn-md sm:w-24">
+            <a className="btn btn-primary btn-sm w-20 text-sm normal-case text-primary-content sm:btn-md sm:w-24 sm:text-base">
               Register
             </a>
           </Link>
