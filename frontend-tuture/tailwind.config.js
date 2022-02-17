aff47d1f8eb4e50aaa9ba5b3b3bc83dd90811d47
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-18 00:57:23 +0700

    [frontend] set tailwind purge

--- frontend-tuture/tailwind.config.js
+++ frontend-tuture/tailwind.config.js
@@ -3,6 +3,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   mode: 'jit',
   darkMode: 'class',
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
