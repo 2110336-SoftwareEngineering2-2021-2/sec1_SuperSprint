a08821e1859a6ca784bb6c66e79c033813dc943d
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-22 02:05:07 +0700

    [register pages] refactoring price range form

--- frontend-tuture/pages/register/tutor.js
+++ frontend-tuture/pages/register/tutor.js
@@ -24,13 +24,12 @@ import {
 } from '../../components/register-pages/Constants';
 import SubjectListForm from '../../components/register-pages/SubjectListForm';
 import AvailabilityListForm from '../../components/register-pages/AvailabilityListForm';
-const { Range } = Slider;
+import PriceRangeForm from '../../components/register-pages/PriceRangeForm';
 
 function TutorRegister({ subjects, avatarSeed }) {
   const [password, setPassword] = useState({ password: '', score: 0 });
   const [avatarFile, setAvatarFile] = useState({ preview: '', name: '' });
   const [firstName, setFirstName] = useState('');
-  const [priceRange, setPriceRange] = useState([2000, 4500]);
   const {
     register,
     handleSubmit,
@@ -62,31 +61,6 @@ function TutorRegister({ subjects, avatarSeed }) {
   //       );
   //     }),
   // });
-
-  function setMinPriceRange(event) {
-    let newPriceRange = [...priceRange];
-    newPriceRange[0] = event.target.value;
-    setPriceRange(newPriceRange);
-  }
-
-  function setMaxPriceRange(event) {
-    let newPriceRange = [...priceRange];
-    newPriceRange[1] = event.target.value;
-    setPriceRange(newPriceRange);
-  }
-
-  function validatePriceRange() {
-    let newPriceRange = [...priceRange];
-    newPriceRange[0] = Math.min(
-      newPriceRange[1],
-      Math.max(newPriceRange[0], MIN_PRICE)
-    );
-    newPriceRange[1] = Math.max(
-      newPriceRange[0],
-      Math.min(newPriceRange[1], MAX_PRICE)
-    );
-    setPriceRange(newPriceRange);
-  }
 
   async function validateForm(event) {
     const total = availFormVals.length;
@@ -449,61 +423,10 @@ function TutorRegister({ subjects, avatarSeed }) {
             subjects={subjects}
             maxSubject={MAX_SUBJECT}
           />
-
           <label htmlFor="price_range" className="label">
             <span className="label-text">Price Range</span>
           </label>
-          <div className="m-auto my-4 w-11/12">
-            <Range
-              id="price_range"
-              min={MIN_PRICE}
-              max={MAX_PRICE}
-              step={50}
-              value={priceRange}
-              allowCross={false}
-              onChange={(val) => setPriceRange(val)}
-              trackStyle={[{ backgroundColor: '#ffc400' }]}
-              handleStyle={[
-                { borderColor: '#ffc400' },
-                { borderColor: '#ffc400' },
-              ]}
-            />
-          </div>
-          <div className="my-2 flex w-full items-center justify-between">
-            <label
-              className="input-group-xs input-group w-5/12 sm:w-3/12"
-              htmlFor="price_min"
-            >
-              <input
-                id="price_min"
-                type="number"
-                value={priceRange[0]}
-                className="min-w-2/3 sm:min-w-1/2 input-bordered input-primary input input-sm w-full"
-                onChange={(event) => setMinPriceRange(event)}
-                onBlur={validatePriceRange}
-                min={MIN_PRICE}
-                max={MAX_PRICE}
-              />
-              <span>THB</span>
-            </label>
-            <span className="select-none">-</span>
-            <label
-              className="input-group-xs input-group right-0 w-5/12 sm:w-3/12"
-              htmlFor="price_max"
-            >
-              <input
-                id="price_max"
-                type="number"
-                value={priceRange[1]}
-                className="min-w-2/3 sm:min-w-1/2 input-bordered input-primary input input-sm w-full"
-                onChange={(event) => setMaxPriceRange(event)}
-                onBlur={validatePriceRange}
-                min={MIN_PRICE}
-                max={MAX_PRICE}
-              />
-              <span>THB</span>
-            </label>
-          </div>
+          <PriceRangeForm hookFormRegister={register} />
           <label className="label" htmlFor="availability">
             <span className="label-text">Availability (max 10)</span>
           </label>
