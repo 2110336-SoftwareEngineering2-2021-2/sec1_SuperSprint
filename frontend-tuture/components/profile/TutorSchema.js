6e08bd4e54607d92430140e47ace5b4a17ba5018
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-02-27 22:28:12 +0700

    [edit profile] add schemas

--- frontend-tuture/components/profile/TutorSchema.js
+++ frontend-tuture/components/profile/TutorSchema.js
@@ -1,5 +1,4 @@
 import * as yup from 'yup';
-import zxcvbn from 'zxcvbn';
 import {
   uppercaseRegex,
   lowercaseRegex,
@@ -7,24 +6,22 @@ import {
   specialCharRegex,
   phoneRegex,
 } from '../commons/Regex';
-import {
-  MAX_USERNAME_LENGTH,
-  MIN_PWD_LENGTH,
-  MAX_PWD_LENGTH,
-} from './Constants';
+import zxcvbn from 'zxcvbn';
+import moment from 'moment';
+
+const MIN_PWD_LENGTH = 8;
+const MAX_PWD_LENGTH = 30;
+const MAX_USERNAME_LENGTH = 30;
 
 yup.addMethod(yup.array, 'unique', function (message) {
   return this.test('unique', message, function (list) {
-    //var mapperred = []
     const mapper = (x) => String(x.subject) + String(x.level);
-    //const mapper = [...list];
     const set = [...new Set(list.map(mapper))];
     const isUnique = list.length === set.length;
     if (isUnique) {
       return true;
     }
     const { path, createError } = this;
-    //console.log(`HHH ${path}`);
     const idx = list.findIndex((l, i) => mapper(l) !== set[i]);
     return this.createError({
       path: `${path}.${idx}`,
@@ -33,13 +30,14 @@ yup.addMethod(yup.array, 'unique', function (message) {
   });
 });
 
-export const studentEditSchema = yup.object().shape({
+export const tutorEditSchema = yup.object().shape({
   username: yup
     .string()
     .max(
       MAX_USERNAME_LENGTH,
       `Username must not exceed ${MAX_USERNAME_LENGTH} characters`
-    ),
+    )
+    .required(),
   new_password: yup.string().when(['new_password_confirm'], {
     is: (new_password_confirm) => new_password_confirm !== '',
     then: yup
@@ -74,15 +72,21 @@ export const studentEditSchema = yup.object().shape({
   }),
   first_name: yup
     .string()
-    .matches(/^[A-Za-z]+$/, 'First name must contain only alphabet'),
+    .matches(/^[A-Za-z]+$/, 'First name must contain only alphabet')
+    .required(),
   last_name: yup
     .string()
-    .matches(/^[A-Za-z]+$/, 'Last name must contain only alphabet'),
+    .matches(/^[A-Za-z]+$/, 'Last name must contain only alphabet')
+    .required(),
   email: yup.string().email(),
   phone: yup
     .string()
-    .matches(phoneRegex, 'Phone number must contain only number'),
-  gender: yup.string().oneOf(['male', 'female', 'non-binary', 'not_specified']),
+    .matches(phoneRegex, 'Phone number must contain only number')
+    .required(),
+  gender: yup
+    .string()
+    .oneOf(['male', 'female', 'non-binary', 'not_specified'])
+    .required(),
   subjects: yup
     .array()
     .of(
@@ -95,8 +99,6 @@ export const studentEditSchema = yup.object().shape({
             function (value, context) {
               const parent2 = context.from[1];
               const len = parent2.value.subjects.length;
-              //console.log(parent2, len);
-              //console.log("DDD",parent1,parent2.value.subjects.length);
               if (value === '' && len === 1) {
                 return true;
               } else if (value !== '') {
@@ -113,8 +115,6 @@ export const studentEditSchema = yup.object().shape({
             function (value, context) {
               const parent2 = context.from[1];
               const len = parent2.value.subjects.length;
-              //console.log(parent2, len);
-              //console.log("DDD",parent1,parent2.value.subjects.length);
               if (value === '' && len === 1 && context.parent.subject === '') {
                 return true;
               } else if (value !== '') {
@@ -126,4 +126,75 @@ export const studentEditSchema = yup.object().shape({
       })
     )
     .unique('Duplicate subject and level'),
+  price: yup.object({
+    min: yup.number().min(0).max(10000),
+    max: yup
+      .number()
+      .min(0)
+      .max(10000)
+      .test(function (value, context) {
+        const price_min = context.parent.min;
+        return value >= price_min;
+      }),
+  }),
+  availability: yup
+    .array()
+    .of(
+      yup.object({
+        from: yup.date().typeError('Type error : from').nullable(true),
+        to: yup.date().typeError('Type error : to').nullable(true),
+      })
+    )
+    .test(
+      'null',
+      'Available time must be provided for added boxes',
+      function (value, context) {
+        var nullCount = 0;
+        value.forEach((e) => {
+          if (!e.from || !e.to) nullCount++;
+        });
+        const A = !!value[0].from;
+        const B = !!value[0].to;
+        return (
+          (value.length !== 1 && nullCount === 0) ||
+          (value.length === 1 && !(A ^ B))
+        );
+      }
+    )
+    .test(
+      'from is less or equal to',
+      'End time must be equal or greater than start time',
+      function (value, context) {
+        var isError = false;
+        value.forEach((e) => {
+          if (!isError && e.from > e.to) isError = true;
+        });
+        return !isError;
+      }
+    )
+    .test(
+      'overlapDate',
+      'Available time must not be overlapped',
+      function (value, context) {
+        const len = value.length;
+        for (var i = 0; i < len; i++) {
+          for (var j = i + 1; j < len; j++) {
+            const is_fj_greater_ti = moment(
+              value[j].from,
+              'HH:mm'
+            ).isSameOrAfter(moment(value[i].to, 'HH:mm'));
+
+            const is_fi_greater_tj = moment(
+              value[i].from,
+              'HH:mm'
+            ).isSameOrAfter(moment(value[j].to, 'HH:mm'));
+
+            if (!is_fi_greater_tj && !is_fj_greater_ti) {
+              return false;
+            }
+          }
+        }
+        return true;
+      }
+    ),
 });
