44903eef358b0a56372891e7289eb1c87068cdaf
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-03-20 17:52:42 +0700

    [frontend tutor score] ScoreEditCard fix styling

--- frontend-tuture/components/tutor-score/ScoreEditCard.js
+++ frontend-tuture/components/tutor-score/ScoreEditCard.js
@@ -41,52 +41,50 @@ export default function ScoreEditCard({
         className="flex w-auto max-w-3xl flex-col justify-between rounded-lg bg-base-100 shadow-lg transition-all hover:shadow-amber-400/40 sm:flex-row"
         tabIndex={0}
       >
-        <div className="inline-flex w-64 flex-col justify-between pt-4 pl-4 pb-4 sm:w-96">
+        <div className="inline-flex w-full flex-col justify-between p-4 sm:w-full">
           <h3 className="text-md w-40 max-w-fit overflow-hidden overflow-ellipsis whitespace-nowrap font-bold text-base-content transition-colors hover:cursor-pointer hover:text-primary-focus xs:w-52 sm:w-80 sm:text-xl">
             {scoreData.subject} ({scoreData.level})
           </h3>
           <div className="divider"></div>
           <div>
-            <label className="input-group mb-2">
+            <label className="input-group input-group-sm mb-2">
               <span className="w-20">Score</span>
               <input
-                className="input input-bordered input-primary input-sm w-32"
+                className="input input-bordered input-primary input-sm w-24 xs:w-32"
                 {...hookFormRegister(`${scoreData.subjectId}.score`)}
                 id="score"
                 type="number"
                 placeholder="0"
                 max={scoreData.maxScore}
               />
-              <span className="w-20">/ {scoreData.maxScore}</span>
+              <span>/ {scoreData.maxScore}</span>
             </label>
-            <label className="input-group">
+            <label className="input-group input-group-sm">
               <span className="w-20">Year</span>
-              <select
-                className="input input-bordered input-primary input-sm w-32"
+              <input
+                className="input input-bordered input-primary input-sm w-24 xs:w-32"
                 {...hookFormRegister(`${scoreData.subjectId}.year`)}
                 id="score"
                 type="text"
                 placeholder="0"
-                defaultValue=""
-              >
-                <option value="" disabled></option>
-              </select>
+                disabled
+              />
             </label>
           </div>
         </div>
-        <div className="flex flex-col items-center space-y-6">
+        <div className="flex flex-col justify-center items-center sm:flex-row gap-1 p-2">
           <ScoreImageUpload
             subjectId={scoreData.subjectId}
             hookFormControl={hookFormControl}
             defaultValue={'https://api.lorem.space/image/shoes?w=320&h=320'}
           />
+          <button
+            className="btn btn-circle btn-ghost btn-sm"
+            onClick={onDelete}
+          >
+            <FontAwesomeIcon icon={faTrash} fixedWidth />
+          </button>
         </div>
-        <button
-          className="btn-fit btn border-none bg-transparent hover:cursor-pointer hover:border-none hover:bg-transparent hover:text-primary-focus"
-          onClick={onDelete}
-        >
-          <FontAwesomeIcon icon={faTrash} fixedWidth />
-        </button>
       </div>
     </Transition>
   );
